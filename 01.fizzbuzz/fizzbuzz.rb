37d3323625f7178e1315b6386c5d2ae191de9f17
dinoueb <dinoueb@gmail.com>
2026-06-10 21:41:51 +0900

    FizzBuzz問題の課題の提出
    1から20までの範囲の数を出力するプログラム。
    ただし以下の倍数の場合は数字の代わりに文字列を出力する
    
    3の倍数のときは｢Fizz｣
    5の倍数のときは｢Buzz｣
    3と5両方の倍数のときには｢FizzBuzz｣

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,8 @@
+def fizzbuzz(number)
+  return "FizzBuzz" if number % 15 == 0
+  return "Fizz" if number % 3 == 0
+  return "Buzz" if number % 5 == 0
+  number
+end
+
+(1..20).each { |number| puts fizzbuzz(number) }
