e6ad881f47cfb663b32a21fcbb1fadb1f260bb66
dinoueb <dinoueb@gmail.com>
2026-06-21 10:54:06 +0900

    メソッド内の変数を定数化

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -3,11 +3,15 @@
 require 'optparse'
 require 'date'
 
+DAYS_OF_WEEK = ["日", "月", "火", "水", "木", "金", "土"]
+MAX_WEEK = 6
+YEAR_FORMAT_WIDTH = 13
+MONTH_FORMAT_WIDTH = 8
+WEEK_FORMAT_WIDTH = 22
+DAY_FORMAT_WIDTH = 2
+
 def create_monthly_calendar(first_day, last_day)
-  days_in_week = 7
-  max_week = 6
-  
-  monthly_calendar = Array.new(max_week) { Array.new(days_in_week) }
+  monthly_calendar = Array.new(MAX_WEEK) { Array.new(DAYS_OF_WEEK.length) }
   
   week_count = 0
   first_day.step(last_day) do |date|
@@ -22,14 +26,12 @@ def create_monthly_calendar(first_day, last_day)
 end
 
 def print_monthly_calendar(monthly_calendar, date)
-  days_of_week = ["日", "月", "火", "水", "木", "金", "土"]
-
   # ヘッダー
-  puts "#{date.month}月".rjust(8) + " " + "#{date.year}".ljust(13) # ljustの右側へのスペースの追加はcalコマンドの出力結果に合わせたため
-  puts days_of_week.join(" ").ljust(15) # ljustの右側へのスペースの追加はcalコマンドの出力結果に合わせたため
+  puts "#{date.month}月".rjust(MONTH_FORMAT_WIDTH) + " " + "#{date.year}".ljust(YEAR_FORMAT_WIDTH) # ljustの右側へのスペースの追加はcalコマンドの出力結果に合わせたため
+  puts DAYS_OF_WEEK.join(" ").ljust(15) # ljustの右側へのスペースの追加はcalコマンドの出力結果に合わせたため
 
   monthly_calendar.each do |week|
-    puts week.map { |day| day.to_s.rjust(2) }.join(" ").ljust(22) # ljustの右側へのスペースの追加はcalコマンドの出力結果に合わせたため
+    puts week.map { |day| day.to_s.rjust(DAY_FORMAT_WIDTH) }.join(" ").ljust(WEEK_FORMAT_WIDTH) # ljustの右側へのスペースの追加はcalコマンドの出力結果に合わせたため
   end
 end
 
