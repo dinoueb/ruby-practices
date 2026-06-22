a9eec6e09e138e980942aa443ced3213a60dc280
dinoueb <dinoueb@gmail.com>
2026-06-22 17:23:56 +0900

    マジックナンバーを定数化

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -9,7 +9,8 @@ CALENDAR_FORMAT_WIDTH = {
   "year" => 13,
   "month" => 8,
   "week" => 22,
-  "day" => 2
+  "day" => 2,
+  "wday" => 15
 }
 
 def create_monthly_calendar(first_day, last_day)
@@ -30,7 +31,7 @@ end
 def print_monthly_calendar(monthly_calendar, date)
   # ヘッダー
   puts "#{date.month}月".rjust(CALENDAR_FORMAT_WIDTH["month"]) + " " + "#{date.year}".ljust(CALENDAR_FORMAT_WIDTH["year"]) # ljustの右側へのスペースの追加はcalコマンドの出力結果に合わせたため
-  puts DAYS_OF_WEEK.join(" ").ljust(15) # ljustの右側へのスペースの追加はcalコマンドの出力結果に合わせたため
+  puts DAYS_OF_WEEK.join(" ").ljust(CALENDAR_FORMAT_WIDTH["wday"]) # ljustの右側へのスペースの追加はcalコマンドの出力結果に合わせたため
 
   monthly_calendar.each do |week|
     puts week.map { |day| day.to_s.rjust(CALENDAR_FORMAT_WIDTH["day"]) }.join(" ").ljust(CALENDAR_FORMAT_WIDTH["week"]) # ljustの右側へのスペースの追加はcalコマンドの出力結果に合わせたため
