d8fe4fe39caca10671cd1e6bc14fbd869263ae9d
dinoueb <dinoueb@gmail.com>
2026-06-23 23:32:50 +0900

    スコア計算の処理をメソッド化

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -5,21 +5,25 @@ FRAME_NUMBER_IN_GAME = 10
 MAX_PINS = 10
 ROLL_RESULT_TO_SCORE = { 'X' => 10 }.freeze
 
-scores = ARGV[0].split(',').map { |roll_result| ROLL_RESULT_TO_SCORE.fetch(roll_result, roll_result.to_i) }
-
-roll_count = 0
-total_score = 0
-FRAME_NUMBER_IN_GAME.times do
-  if scores[roll_count] == MAX_PINS
-    total_score += scores[roll_count, 3].sum
-    roll_count += 1
-  elsif scores[roll_count, 2].sum == MAX_PINS
-    total_score += scores[roll_count, 3].sum
-    roll_count += 2
-  else
-    total_score += scores[roll_count, 2].sum
-    roll_count += 2
+def calc_total_score(scores)
+  roll_count = 0
+  total_score = 0
+  FRAME_NUMBER_IN_GAME.times do
+    if scores[roll_count] == MAX_PINS
+      total_score += scores[roll_count, 3].sum
+      roll_count += 1
+    elsif scores[roll_count, 2].sum == MAX_PINS
+      total_score += scores[roll_count, 3].sum
+      roll_count += 2
+    else
+      total_score += scores[roll_count, 2].sum
+      roll_count += 2
+    end
   end
+
+  total_score
 end
 
-puts total_score
+scores = ARGV[0].split(',').map { |roll_result| ROLL_RESULT_TO_SCORE.fetch(roll_result, roll_result.to_i) }
+
+puts calc_total_score(scores)
