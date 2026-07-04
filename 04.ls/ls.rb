718456b61bad6d707a4bb5a726df6613ec59be85
dinoueb <dinoueb@gmail.com>
2026-07-04 22:21:09 +0900

    オプションを取らないlsコマンドの実装

---
+++ 04.ls/ls.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+COLUMN_WIDTH_MULTIPLIER = 8
+MAX_COLUMN = 3
+
+def calc_column_width(files)
+  max_filename_length = files.map(&:length).max
+  if (max_filename_length % COLUMN_WIDTH_MULTIPLIER).zero?
+    max_filename_length
+  else
+    (max_filename_length / COLUMN_WIDTH_MULTIPLIER + 1) * COLUMN_WIDTH_MULTIPLIER
+  end
+end
+
+def print_files(files)
+  column_width = calc_column_width(files)
+  max_row = (files.length + (MAX_COLUMN - 1)) / MAX_COLUMN
+  max_row.times do |row_number|
+    MAX_COLUMN.times do |column_number|
+      index = row_number + max_row * column_number
+      break if index >= files.length
+
+      if column_number != MAX_COLUMN - 1
+        print files[index].ljust(column_width)
+      else
+        print files[index]
+      end
+    end
+    puts
+  end
+end
+
+target_files = Dir.glob('*')
+
+print_files(target_files) unless target_files.empty?
